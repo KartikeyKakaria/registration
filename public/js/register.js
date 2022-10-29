03ad8fd6c3f71c39bbf286ba9f0632d92cf1eccf
JaiSharmaTech <kartikeykakaria5@gmail.com>
2022-10-29 00:55:42 -0700

    applied ajax to register will now do for login

--- public/js/register.js
+++ public/js/register.js
@@ -1,9 +1,33 @@
 window.onload = ()=>{
-    const name = document.querySelector("#name")
-    const email = document.querySelector("email")
-    const number = document.querySelector("number")
-    const address = document.querySelector("address")
-    const age = document.querySelector("age")
-    const password = document.querySelector("password")
-    const confpassword = document.querySelector("confpassword")
+    document.getElementById('submit').addEventListener('click',(e)=>{
+        e.preventDefault();
+        const name = document.querySelector("#name").value;
+        const email = document.querySelector("#email").value;
+        const number = document.querySelector("#number").value;
+        const address = document.querySelector("#address").value;
+        const age = document.querySelector("#age").value;
+        const password = document.querySelector("#password").value;
+        const confpassword = document.querySelector("#confpassword").value;
+        const data = {
+            name:name,
+            email:email,
+            number:number,
+            address:address,
+            age:age,
+            password:password,
+            confpassword:confpassword,
+        }
+        const params = {
+            method:'post',
+            headers:{
+                'Content-type':'application/json'
+            },
+            body:JSON.stringify(data),
+        }
+        fetch("/register",params)
+        .then(response => response.text())
+        .then(data =>console.log(data))
+        .catch(err =>console.log(err))
+        // console.log(data)
+    })
 }
