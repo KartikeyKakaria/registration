68c6982b7082c5cbb5c1eb24e7d3a2a47f8f93a3
JaiSharmaTech <kartikeykakaria5@gmail.com>
2022-10-29 00:24:44 -0700

    started with ajax

---
+++ public/js/register.js
@@ -0,0 +1,9 @@
+window.onload = ()=>{
+    const name = document.querySelector("#name")
+    const email = document.querySelector("email")
+    const number = document.querySelector("number")
+    const address = document.querySelector("address")
+    const age = document.querySelector("age")
+    const password = document.querySelector("password")
+    const confpassword = document.querySelector("confpassword")
+}
