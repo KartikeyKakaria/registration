9b6ec0730ee68a93979512b992a091509da17502
KartikeyKakaria <kartikeykakaria2@gmail.com>
2022-11-01 20:32:09 +0530

    now the user can see his etails on the user page

--- public/js/user.js
+++ public/js/user.js
@@ -5,7 +5,13 @@ window.onload = () => {
                 method: 'post'
             })
             .then(rep => rep.json())
-            .then(data => console.log(data))
+            .then(data => {
+                document.getElementById("name").innerText = data.name
+                document.getElementById("age").innerText = data.age
+                document.getElementById("number").innerText = data.number
+                document.getElementById("email").innerText = data.email
+                document.getElementById("address").innerText = data.address
+            })
             .catch(err => console.log(err))
     } else {
         window.location.href = "/login";
