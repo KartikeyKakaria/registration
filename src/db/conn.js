60856b582ef92a0eb04984a257b31fc5c7de1125
KartikeyKakaria <81517323+KartikeyKakaria@users.noreply.github.com>
2022-11-12 18:34:46 +0530

    Test for github dev

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/Users", {
+mongoose.connect(process.env.DB, {
         useUnifiedTopology: true,
         useNewUrlParser: true,
     }).then(() => {
